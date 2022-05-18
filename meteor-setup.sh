62786ca2ff22bc09acf1c6a26b3775067774176c
Netsu <52031254+WeebNetsu@users.noreply.github.com>
2022-05-18 21:07:39 +0200

    Added ts and roles

--- meteor-setup.sh
+++ meteor-setup.sh
@@ -2,10 +2,12 @@
 
 # this script is used to generate a basic meteor project and adding packages often used with it
 
-meteor create $1
+# meteor create $1
+meteor create --typescript $1
 cd $1
-meteor add react-meteor-data accounts-password
+meteor add react-meteor-data accounts-password alanning:roles
 meteor remove insecure autopublish
+# bcrypt is required by meteor encryption
 meteor npm install --save bcrypt
 
 if [ $2 = "mobile" ]; then
@@ -14,4 +16,5 @@ fi
 
 # things we could also add
 # allow user to select js lib (svelt, vue, react etc.)
+# allow user to choose between typescript template or normal js
 # add MUI meteor npm install @mui/material @emotion/react @emotion/styled @mui/icons-material
